07fbbe0fc6cdbbd56bca32218e2cb8ce3b29eefd
Yves Kaufmann <fxdapokalypse@googlemail.com>
2015-05-21 19:10:43 +0200

    Prevent the JlineConsoleReader from closing.

--- Server/src/main/java/de/htw/vs/server/commands/UDPEchoCommands.java
+++ Server/src/main/java/de/htw/vs/server/commands/UDPEchoCommands.java
@@ -1,9 +1,12 @@
 package de.htw.vs.server.commands;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.PrintStream;
 
+import jline.TerminalSupport;
+import jline.console.ConsoleReader;
+
+import org.fusesource.jansi.AnsiConsole;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.stereotype.Component;
@@ -16,18 +19,22 @@ public class UDPEchoCommands implements CommandMarker {
 	
 	@CliCommand(value= "udp echo", help="Starts an echo server and a echo client")
 	public void echoServer() throws IOException {
+		
+		PrintStream out = AnsiConsole.out;
+		ConsoleReader reader = new ConsoleReader(System.in, out, new TerminalSupport(true) {});
 		UDPEchoServer server = new UDPEchoServer();
 		UDPEchoClient client = new UDPEchoClient();
-		try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+		try {
 			server.start();
 			client.start();
+			
 			String line = null;
 			while((line = reader.readLine().trim()) != "" && line.length() > 0) {
 				client.sendMessage(line);
 				String answer = null;
 				
 				while((answer = client.recvMessage()) != null) {
-					System.out.println(answer);
+					out.println(answer);
 				}
 			}
 		} catch(Exception ex) {
